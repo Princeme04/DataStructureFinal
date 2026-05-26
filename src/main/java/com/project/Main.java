ad8bd730c7eea089779e23cdb3d0123a52b7257d
amaliaauliaa <amaliasyt@gmail.com>
2026-05-26 14:27:29 +0700

    well well well

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,6 +2,6 @@ package com.project;
 
 public class Main {
     static void main() {
-        System.out.println("Welcome to my Shitt!!!!!");
+        System.out.println("Welcome to my Shitt!!!!!HAHAHAHHHAHA");
     }
 }
