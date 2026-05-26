97519e1c322d0510749fa1d3f43b019d8f0a4d2b
amaliaauliaa <amaliasyt@gmail.com>
2026-05-26 14:26:14 +0700

    nyoba commit

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -2,7 +2,7 @@ package com.project;
 
 public class Main {
     static void main() {
-        System.out.println("Welcome to hhhhhmy Shitt!!!!!");
+        System.out.println("Welcome to hhhmy Shitt!!!!!");
     }
 
     //nyoba commit
